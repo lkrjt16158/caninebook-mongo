48756cda321fad9c0f5faff7a0d9b1f55117a3c5
Lokender Singh <lokendersingh@Lokenders-MacBook-Air.local>
2022-01-25 21:04:56 +0530

    first commit

---
+++ src/main/java/com/loki/caninebookmongo/CaninebookMongoApplication.java
@@ -0,0 +1,13 @@
+package com.loki.caninebookmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaninebookMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaninebookMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loki/caninebookmongo/CaninebookMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loki.caninebookmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaninebookMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
